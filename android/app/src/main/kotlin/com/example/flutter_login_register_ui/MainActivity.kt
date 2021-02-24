1b3a766a5ca7570e59cb7c2aba4a5cb6243ff6c3
Tony David <tonydavidx@gmail.com>
2021-02-24 11:56:15 +0530

    starter project

---
+++ android/app/src/main/kotlin/com/example/flutter_login_register_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_login_register_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const kBackgroundColor = Color(0xff191720);
+const kTextFieldFill = Color(0xff1E1C24);
+// TextStyles
+const kHeadline = TextStyle(
+  color: Colors.white,
+  fontSize: 34,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyText = TextStyle(
+  color: Colors.grey,
+  fontSize: 15,
+);
+
+const kButtonText = TextStyle(
+  color: Colors.black87,
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyText2 =
+    TextStyle(fontSize: 28, fontWeight: FontWeight.w500, color: Colors.white);
